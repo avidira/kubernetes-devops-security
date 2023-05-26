d14c1dd236d2f239dc9b4112e243e15f93dde01f
vidira solution <vidira.solutions@gmail.com>
2023-05-26 20:43:51 +0000

    DevSecOps Basic Files

---
+++ setup/vm-install-script/java_setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo ".........----------------#################._.-.-INSTALL-.-._.#################----------------........."
+PS1='\[\e[01;36m\]\u\[\e[01;37m\]@\[\e[01;33m\]\H\[\e[01;37m\]:\[\e[01;32m\]\w\[\e[01;37m\]\$\[\033[0;37m\] '
+echo "PS1='\[\e[01;36m\]\u\[\e[01;37m\]@\[\e[01;33m\]\H\[\e[01;37m\]:\[\e[01;32m\]\w\[\e[01;37m\]\$\[\033[0;37m\] '" >> ~/.bashrc
+sed -i '1s/^/force_color_prompt=yes\n/' ~/.bashrc
+source ~/.bashrc
+
+echo ".........----------------#################._.-.-Java and MAVEN-.-._.#################----------------........."
+sudo apt install software-properties-common -y
+sudo apt install default-11-jre -y
+sudo apt install openjdk-11-jdk -y
+java -version
+javac -version
+
+echo ".........----------------#################._.-.-COMPLETED-.-._.#################----------------........."
---
+++ setup/vm-install-script/jenkins_setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo ".........----------------#################._.-.-INSTALL-.-._.#################----------------........."
+PS1='\[\e[01;36m\]\u\[\e[01;37m\]@\[\e[01;33m\]\H\[\e[01;37m\]:\[\e[01;32m\]\w\[\e[01;37m\]\$\[\033[0;37m\] '
+echo "PS1='\[\e[01;36m\]\u\[\e[01;37m\]@\[\e[01;33m\]\H\[\e[01;37m\]:\[\e[01;32m\]\w\[\e[01;37m\]\$\[\033[0;37m\] '" >> ~/.bashrc
+sed -i '1s/^/force_color_prompt=yes\n/' ~/.bashrc
+source ~/.bashrc
+
+echo ".........----------------#################._.-.-JENKINS-.-._.#################----------------........."
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+# wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+# sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install -y jenkins
+sudo systemctl daemon-reload
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+# sudo systemctl status jenkins
+#sudo usermod -a -G jenkins
+echo "jenkins ALL=(ALL) NOPASSWD: ALL" | sudo tee -a /etc/sudoers > /dev/null
+
+echo ".........----------------#################._.-.-COMPLETED-.-._.#################----------------........."
